1ebf805ae21bdae8e19e0e972d382eb49427e408
David Svensson <davidsvson@gmail.com>
2020-04-02 11:43:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lektion1android/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.lektion1android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editText : EditText
+
+    val TAG = MainActivity::class.java.simpleName
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.inputText)
+        Log.d(TAG,"Hej")
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.text = "Tryck"
+
+        button.setOnClickListener { v ->
+            val message = editText.text.toString()
+            Log.d(TAG,  message )
+        }
+
+
+    }
+
+    /*
+    fun buttonPressed(view : View) {
+        val message = editText.text.toString()
+        Log.d(TAG,  message )
+    }
+*/
+
+}
